56623ac684f1d2368c50efc2bcb2070a96d1fca5
willisc22 <scwilliams15@gmail.comgitconfig --globsl user.name willisc22>
2024-02-17 14:09:01 -0600

    Class Reviewed

--- scripts/prescribers_script.sql
+++ scripts/prescribers_script.sql
@@ -1,8 +1,9 @@
 --Question1 Which prescriber had the highest total number of claims (totaled over all drugs)? Report the npi and the total number of claims.
- SELECT prescription.npi, SUM(prescription.total_claim_count)
+ SELECT prescription.npi, 
+ 	SUM(prescription.total_claim_count) AS total_claims
  FROM prescription
  GROUP BY prescription.npi
- ORDER BY SUM(prescription.total_claim_count) DESC
+ ORDER BY total_claims DESC
  LIMIT 3;
  
 
@@ -21,6 +22,20 @@
  prescriber.nppes_provider_last_org_name,
  prescriber.specialty_description
  ORDER BY SUM(prescription.total_claim_count) DESC
+ LIMIT 3;
+--using AS below
+ SELECT p2.npi,
+ p1.nppes_provider_first_name,
+ p1.nppes_provider_last_org_name,
+ p1.specialty_description, 
+ SUM(p2.total_claim_count)
+ FROM prescriber AS p1
+ 	INNER JOIN prescription AS p2
+	ON p1.npi = p2.npi
+ GROUP BY p2.npi, p1.nppes_provider_first_name,
+ p1.nppes_provider_last_org_name,
+ p1.specialty_description
+ ORDER BY SUM(p2.total_claim_count) DESC
  LIMIT 3;
 
 --1b answer: 1881634483	"BRUCE"	"PENDLEY"	"Family Practice"	99707
@@ -65,18 +80,71 @@ SELECT
  	HAVING SUM(prescription.total_claim_count) IS NULL;
  
  --Question2d d. **Difficult Bonus:** *Do not attempt until you have solved all other problems!* For each specialty, report the percentage of total claims by that specialty which are for opioids. Which specialties have a high percentage of opioids?
- 
- WITH opioid_claims AS (
- 	SELECT SUM(prescription.total_claim_count) AS opioid_total_claim
-					FROM prescription
-		 			INNER JOIN drug
-		 				ON prescription.drug_name = drug.drug_name
-		 			INNER JOIN prescriber
-		 				ON prescription.npi = prescriber.npi
-					WHERE drug.opioid_drug_flag = 'Y'
-				GROUP BY prescriber.specialty_description)-- CTE for total opioid claims
-claims AS 
-S
+--CASE WHEN APPROACH
+SELECT
+	specialty_description,
+	SUM(
+		CASE WHEN opioid_drug_flag = 'Y' THEN total_claim_count
+		ELSE 0
+	END
+	) as opioid_claims,
+	
+	SUM(total_claim_count) AS total_claims,
+	
+	SUM(
+		CASE WHEN opioid_drug_flag = 'Y' THEN total_claim_count
+		ELSE 0
+	END
+	) * 100.0 /  SUM(total_claim_count) AS opioid_percentage
+FROM prescriber
+INNER JOIN prescription
+USING(npi)
+INNER JOIN drug
+USING(drug_name)
+GROUP BY specialty_description
+--order by specialty_description;
+order by opioid_percentage desc
+
+---CTE APPROACH
+WITH claims AS 
+	(SELECT
+		pr.specialty_description,
+		SUM(rx.total_claim_count) AS total_claims
+	FROM prescriber AS pr
+	INNER JOIN prescription AS rx
+	USING(npi)
+	INNER JOIN drug
+	USING (drug_name)
+	GROUP BY pr.specialty_description),
+-- second CTE for total opioid claims
+opioid AS
+	(SELECT
+		pr.specialty_description,
+		SUM(rx.total_claim_count) AS total_opioid
+	FROM prescriber AS pr
+	INNER JOIN prescription AS rx
+	USING(npi)
+	INNER JOIN drug
+	USING (drug_name)
+	WHERE drug.opioid_drug_flag ='Y'
+	GROUP BY pr.specialty_description)
+--main query
+SELECT
+	claims.specialty_description,
+	COALESCE(ROUND((opioid.total_opioid / claims.total_claims * 100),2),0) AS perc_opioid
+FROM claims
+LEFT JOIN opioid
+USING(specialty_description)
+ORDER BY perc_opioid DESC;
+
+--Question3a Which drug (generic_name) had the highest total drug cost?
+SELECT drug.generic_name,
+ SUM(prescription.total_drug_cost)
+ FROM prescription
+ 	INNER JOIN drug
+	ON prescription.drug_name = drug.drug_name
+GROUP BY drug.generic_name
+ORDER BY SUM(prescription.total_drug_cost) DESC;
 
 --Question3a answer "INSULIN GLARGINE,HUM.REC.ANLOG"	104264066.35
 
@@ -119,6 +187,15 @@ WHERE cbsaname LIKE '%TN%';
 
 --Question5a answer: 56
 
+--reviewed/correct5a
+SELECT COUNT (*)
+FROM cbsa
+INNER JOIN fips_county
+USING (fipscounty)
+WHERE state = 'TN'
+
+--Correct, 42
+
 --Question5b Which cbsa has the largest combined population? Which has the smallest? Report the CBSA name and total population.
 SELECT SUM(population.population), cbsa.cbsaname
 FROM population
@@ -138,6 +215,8 @@ FROM population
 	WHERE cbsa IS NULL
 	ORDER BY population.population DESC;
 	
+--Question5c answer 95523	"SEVIER"
+
 --Question 6a Find all rows in the prescription table where total_claims is at least 3000. Report the drug_name and the total_claim_count.
 SELECT drug_name,
 	total_claim_count
